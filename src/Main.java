68a0e4f6b3fdc5e0e969c689762b3ef7f955e687
Arkadiusz Mitura <amitura@protonmail.com>
2018-10-06 14:03:48 +0200

    Second2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String ... args) {
-        System.out.println("Hej"+ args.length);
+        System.out.println("Hej 2"+ args.length);
         System.out.println(gitMap());
 
 
