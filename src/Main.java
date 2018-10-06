55ce56b98036ca1384e748b920a3923fb4a63570
Arkadiusz Mitura <amitura@protonmail.com>
2018-10-06 12:58:28 +0200

    Second

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String ... args) {
         System.out.println("Hej");
         System.out.println(2);
+
+
     }
+    private static String gitMap(){
+        return "haslo";
+    }
+
+
 
 }
